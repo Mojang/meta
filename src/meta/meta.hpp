e51974e22ddc635d630cc49ad9537872e942244f
Tyler Nilsson <tyler.nilsson@blackbirdinteractive.com>
2020-09-14 11:20:14 -0700

    Added invoke_result

--- src/meta/meta.hpp
+++ src/meta/meta.hpp
@@ -25,6 +25,61 @@ class data;
 class func;
 class type;
 
+namespace detail {
+	template <class T>
+	struct is_reference_wrapper : std::false_type { };
+	template <class U>
+	struct is_reference_wrapper<std::reference_wrapper<U>> : std::true_type { };
+
+	template<class T>
+	struct invoke_impl {
+		template<class F, class... Args>
+		static auto call(F&& f, Args&&... args)
+			-> decltype(std::forward<F>(f)(std::forward<Args>(args)...));
+	};
+
+	template<class B, class MT>
+	struct invoke_impl<MT B::*> {
+		template<class T, class Td = typename std::decay<T>::type,
+			class = typename std::enable_if<std::is_base_of<B, Td>::value>::type
+		>
+			static auto get(T&& t)->T&&;
+
+		template<class T, class Td = typename std::decay<T>::type,
+			class = typename std::enable_if<is_reference_wrapper<Td>::value>::type
+		>
+			static auto get(T&& t) -> decltype(t.get());
+
+		template<class T, class Td = typename std::decay<T>::type,
+			class = typename std::enable_if<!std::is_base_of<B, Td>::value>::type,
+			class = typename std::enable_if<!is_reference_wrapper<Td>::value>::type
+		>
+			static auto get(T&& t) -> decltype(*std::forward<T>(t));
+
+		template<class T, class... Args, class MT1,
+			class = typename std::enable_if<std::is_function<MT1>::value>::type
+		>
+			static auto call(MT1 B::*pmf, T&& t, Args&&... args)
+			-> decltype((invoke_impl::get(std::forward<T>(t)).*pmf)(std::forward<Args>(args)...));
+
+		template<class T>
+		static auto call(MT B::*pmd, T&& t)
+			-> decltype(invoke_impl::get(std::forward<T>(t)).*pmd);
+	};
+
+	template<class F, class... Args, class Fd = typename std::decay<F>::type>
+	auto INVOKE(F&& f, Args&&... args)
+		-> decltype(invoke_impl<Fd>::call(std::forward<F>(f), std::forward<Args>(args)...));
+
+	template <typename AlwaysVoid, typename, typename...>
+	struct invoke_result { };
+	template <typename F, typename...Args>
+	struct invoke_result<decltype(void(detail::INVOKE(std::declval<F>(), std::declval<Args>()...))),
+		F, Args...> {
+		using type = decltype(detail::INVOKE(std::declval<F>(), std::declval<Args>()...));
+	};
+} // namespace detail
+
 template<typename Fn, typename... Args,
 	std::enable_if_t < std::is_member_pointer<std::decay_t<Fn>>{}, int > = 0 >
 	constexpr decltype(auto) custom_invoke(Fn&& f, Args&&... args)
@@ -38,6 +93,16 @@ template<typename Fn, typename... Args,
 	noexcept(noexcept(std::forward<Fn>(f)(std::forward<Args>(args)...))) {
 	return std::forward<Fn>(f)(std::forward<Args>(args)...);
 }
+
+template <class> struct result_of;
+template <class F, class... ArgTypes>
+struct result_of<F(ArgTypes...)> : detail::invoke_result<void, F, ArgTypes...> { };
+
+template <class F, class... ArgTypes>
+struct invoke_result : detail::invoke_result<void, F, ArgTypes...> { };
+
+template< class F, class... ArgTypes>
+using invoke_result_t = typename invoke_result<F, ArgTypes...>::type;
 
 template< class T >
 inline constexpr bool is_copy_constructible_v = std::is_copy_constructible<T>::value;
